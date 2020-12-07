17bca2784f83aa1b480ea4cdec6fea505911170e
ammiratafabiano <ammiratafabiano@gmail.com>
2020-12-07 03:25:20 +0100

    back to capacitor

--- Mobile/src/app/home/home.page.ts
+++ Mobile/src/app/home/home.page.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+
+export class CardSetModel {
+  id: number;
+  name: string;
+  size: number;
+}
 
 @Component({
   selector: 'app-home',
@@ -7,6 +14,52 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  code: string;
+  nickname: string;
+  selectedSet = 0;
+
+  cardSets: CardSetModel[];
+
+  constructor(public alertController: AlertController) {
+    this.getCardSets();
+  }
+
+  private join() {
+    if (!this.code) {
+      this.presentAlert('Please, insert a group code.');
+      return;
+    }
+    if (!this.nickname) {
+      this.presentAlert('Please, insert your nickname.');
+      return;
+    }
+  }
+
+  private create() {
+    if (!this.nickname) {
+      this.presentAlert('Please, insert your nickname.');
+      return;
+    }
+  }
+
+  private getCardSets() {
+    this.cardSets = [
+      {
+        id: 0,
+        name: 'Siciliane',
+        size: 40
+      }
+    ];
+  }
+
+  async presentAlert(msg: string) {
+    const alert = await this.alertController.create({
+      header: 'Wait!',
+      message: msg,
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
 
 }
