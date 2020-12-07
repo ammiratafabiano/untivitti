d830a8bdb9dbc53dffe3ac1d841033c497d15f08
ammiratafabiano <ammiratafabiano@gmail.com>
2020-12-07 12:37:33 +0100

    added some root in server

--- Server/server.js
+++ Server/server.js
@@ -2,10 +2,34 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+let groups = []
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Server untivitti.\nStatus: Ok')
+})
+app.get('/getCode', (req, res) => {
+  const group = {
+    code: 'ABCDE',
+    players: [
+      {
+        name: 'fabiam93',
+        isAdmin: true
+      },
+      {
+        name: 'germano98',
+        isAdmin: false
+      }
+    ]
+  }
+  groups.push(group)
+  res.send(group.code)
+})
+app.get('/getState/:code', (req, res) => {
+  const group = groups.find(x => x.code == req.params['code'])
+  res.send(group)
 })
 
-app.listen(port, () => {
+app.listen(port, (err) => {
+  if (err) console.log(err); 
   console.log(`Example app listening at http://localhost:${port}`)
 })
