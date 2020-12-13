89b7cebb90cb39048e16a2b92de5ee5039bcb4c0
ammiratafabiano <ammiratafabiano@gmail.com>
2020-12-13 02:21:09 +0100

    bug fixes

--- Mobile2/untivitti/src/app/app-routing.module.ts
+++ Mobile2/untivitti/src/app/app-routing.module.ts
@@ -3,16 +3,16 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'untivitti',
+    path: 'home',
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
   {
-    path: 'untivitti/join/:code',
+    path: 'join/:code',
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
   {
     path: '',
-    redirectTo: 'untivitti',
+    redirectTo: 'home',
     pathMatch: 'full'
   },
   {
--- Mobile2/untivitti/src/app/services/api.service.ts
+++ Mobile2/untivitti/src/app/services/api.service.ts
@@ -12,7 +12,8 @@ import { ResponseModel } from '../models/response.model';
 })
 export class ApiService {
 
-  ip = '2.238.108.96';
+  // ip = '2.238.108.96';
+  ip = 'localhost';
   domain = 'ammireto.cloud';
   endpoint = 'http://' + this.ip + ':3000';
   clientEndpoint = 'http://' +  this.domain + ':8100';
