08ef14a41cb4f54932350f8ad3b6467ec28544ba
ammiratafabiano <ammiratafabiano@gmail.com>
2020-12-17 22:14:28 +0100

    fix

--- Server/server/server.ts
+++ Server/server/server.ts
@@ -63,7 +63,7 @@ const games = [
         name: 'Cucù!',
         id: 3,
         disabled: false,
-        icon: 'albums-outline',
+        icon: 'hand-left-outline',
         side: 'bottom',
         status: true,
         forbiddenCards: [0,1,2,3,4,5,6,7,8,10,11,12,13,14,15,16,17,18,20,21,22,23,24,25,26,27,28,30,31,32,33,34,35,36,37,38],
