a8a0d0614e34f5aa0e508558e7665318b1b9dbf1
ammiratafabiano <ammiratafabiano@gmail.com>
2020-12-18 18:52:33 +0100

    fix

--- Server/server/server.ts
+++ Server/server/server.ts
@@ -38,6 +38,7 @@ const games = [
         id: 0,
         disabled: false,
         icon: 'share-outline',
+        rotateIcon: false,
         side: 'top',
         status: false
       },
@@ -46,6 +47,7 @@ const games = [
         id: 1,
         disabled: false,
         icon: 'download-outline',
+        rotateIcon: false,
         side: 'top',
         status: true
       },
@@ -54,6 +56,7 @@ const games = [
         id: 2,
         disabled: false,
         icon: 'albums-outline',
+        rotateIcon: true,
         side: 'bottom',
         status: false
       }
@@ -64,6 +67,7 @@ const games = [
         id: 3,
         disabled: false,
         icon: 'hand-left-outline',
+        rotateIcon: false,
         side: 'bottom',
         status: true,
         forbiddenCards: [0,1,2,3,4,5,6,7,8,10,11,12,13,14,15,16,17,18,20,21,22,23,24,25,26,27,28,30,31,32,33,34,35,36,37,38],
@@ -74,6 +78,7 @@ const games = [
         id: 4,
         disabled: false,
         icon: 'thumbs-up-outline',
+        rotateIcon: false,
         side: 'start',
         status: true,
         forbiddenCards: [9,19,29,39],
@@ -84,6 +89,7 @@ const games = [
         id: 5,
         disabled: false,
         icon: 'repeat-outline',
+        rotateIcon: false,
         side: 'end',
         status: true,
         forbiddenCards: [9,19,29,39],
@@ -709,6 +715,7 @@ function getPlayerMoves(group) {
       id: move.id,
       disabled: false,
       icon: move.icon,
+      rotateIcon: move.rotateIcon,
       side: move.side,
       status: move.status,
       forbiddenCards: move.forbiddenCards,
@@ -727,6 +734,7 @@ function getAdminMoves(group) {
       id: move.id,
       disabled: false,
       icon: move.icon,
+      rotateIcon: move.rotateIcon,
       side: move.side,
       status: move.status
     })
