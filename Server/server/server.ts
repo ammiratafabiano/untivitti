8ca98dfc6987df45acef212c8d81fcb597809276
ammiratafabiano <ammiratafabiano@gmail.com>
2020-12-18 23:25:02 +0100

    fix

--- Server/server/server.ts
+++ Server/server/server.ts
@@ -776,5 +776,7 @@ function setAdmin(group, reset = false) {
       const icon = 'Admin'
       sendNotification(group, text, icon)
     }
+  } {
+    group.players.find(x => x.isAdmin == true).isAdmin = false
   }
 }
